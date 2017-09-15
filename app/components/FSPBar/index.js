66c8c28317d70b1a655ab89d17f81d78d45a17e9
Timothy Emmanuel Kasasa <ekastimo@gmail.com>
2017-09-15 15:55:48 +0300

    Added information icon

--- app/components/FSPBar/index.js
+++ app/components/FSPBar/index.js
@@ -4,6 +4,7 @@
  */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Modal from 'react-modal'
 import 'rc-slider/assets/index.css'
 import './style.css'
 import RangeSelector from '../RangeSelector'
@@ -13,15 +14,41 @@ import * as MapActions from '../../actions/map'
 import * as StatsActions from '../../actions/stats'
 import { controls as config } from '../../settings/fspSettings'
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
+
 class FSPBar extends Component {
   constructor (props) {
     super(props)
     const {routeParams: {country, question}} = props
     this.state = {
       data: config[country][question],
-      bankSortOrder: undefined
+      bankSortOrder: undefined,
+      isOpen: false,
     }
     this.mySelectors = {}
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+  }
+
+  openModal () {
+    this.setState({
+      isOpen: true,
+    })
+  }
+
+  closeModal () {
+    this.setState({
+      isOpen: false,
+    })
   }
 
   onRangeChanged (country, question, id, selection, category) {
@@ -61,11 +88,43 @@ class FSPBar extends Component {
   render () {
     const {routeParams: {country, question}, stats} = this.props
     const {sortedData, sortId} = stats.sortOrder || {}
-    const {title, controls} = config[country][question]
+    const {title, controls,tooltip:{body}} = config[country][question]
+
     const percent = controls.length === 4 ? 20 : (controls.length === 3 ? 30 : 40)
+    const InfoBtn = () => <label ref={ref => {this.info = ref}} style={{
+      fontSize: 14,
+      display: 'inline-block',
+      color: 'white',
+      width: 12,
+      height: 16,
+      border: '2px solid white',
+      borderRadius: 15,
+      padding: '0 4px 5px',
+      textAlign: 'center',
+      cursor: 'pointer'
+    }}
+     onClick={() => {
+       this.openModal()
+     }}
+    >{'i'}</label>
     return <div id="fspbar" style={{overflow: 'auto'}}>
+      <Modal
+        isOpen={this.state.isOpen}
+        style={customStyles}
+        aria={{
+          labelledby: 'heading',
+          describedby: 'full_description'
+        }}>
+
+        <h4 id="heading" style={{marginTop: 0, marginBottom: 3}}>Information</h4>
+        <div id="full_description" style={{maxWidth: 400}} dangerouslySetInnerHTML={{ __html: body }}/>
+        <div>
+          <button onClick={this.closeModal} style={{float: 'right'}}>Close</button>
+        </div>
+      </Modal>
       <div style={{color: 'white', fontWeight: 'bold', fontSize: 20, padding: 15, paddingBottom: 0}}>
-        <span>{title}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{fontSize: 14, fontWeight: 'normal'}}>Disclaimer: <i>todo get this from Alyssa</i></span>
+        <span>{title}</span>&nbsp;&nbsp;<InfoBtn/>&nbsp;&nbsp;&nbsp;&nbsp;<span
+        style={{fontSize: 14, fontWeight: 'normal'}}>Disclaimer: <i>todo get this from Alyssa</i></span>
       </div>
       <div style={{
         display: 'flex',
